8f5405ec9f6098d8eb0db7813e0148e0c1e895f7
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2020-12-27 21:02:06 +0200

    Draft

--- sample/src/main/kotlin/dev/ahmedmourad/validation/sample/Main.kt
+++ sample/src/main/kotlin/dev/ahmedmourad/validation/sample/Main.kt
@@ -1,18 +1,31 @@
 package dev.ahmedmourad.validation.sample
 
 import dev.ahmedmourad.validation.core.*
+import dev.ahmedmourad.validation.sample.validations.*
 
 fun main() {
 
 }
 
-data class Password(val v: String) {
+
+data class Rand<T : List<*>>(val v: List<T>?)
+
+data class Nested(val x: String)
+
+data class Password(val v: String, val n: Nested?) {
     companion object : Constrains<Password> {
-        override val constraints = constraints {
-            constraint(violation = "TooShort") {
+        override val constraints by describe {
+            constraint("TooShort") {
+                on(Password::n) {
+//                    on(Nested::x) {
+//
+//                    }
+                }
                 param("min") { 7 }
+                param("max") { Rand(emptyList()) }
+                param("len") { 56.567 }
                 on(Password::v) {
-                    maxLength { 15 }
+                    maxLength { it.toInt() }
                     minLength(7)
                     //it's not final whether the params will be a lambda or a value, probably will provide both options
                     /* or */
@@ -25,28 +38,36 @@ data class Password(val v: String) {
                     v.length in 7..15
                 }
             }
+            val x = "aaa"
             constraint(violation = "ContainsNoDigits") {
                 //...
             }
+//            constraint(violation = "ContainsNoDigits$z") {
+//                //...
+//            }
+//            constraint(violation = "ContainsNoDigits + " /*+ this@Companion.*/) {
+//                //...
+//            }
+//            constraint(violation = "ContainsNoDigits + " + "x") {
+//                //...
+//            }
         }
     }
 }
 
+class RandConstrainer<T : List<*>>(val x: String, val m: T, val c: List<T>, d: Int) : Constrains<Rand<T>> {
+    override val constraints by describe {
+        constraint("ad") {
+
+        }
+    }
+    companion object
+}
+
 //Custom validations
-fun <T : Any> ValidatorBuilder<T, String>.maxLength(max: () -> Int) = validation {
-    length <= max()
+fun <T : Any> ValidatorBuilder<T, String>.maxLength(max: (String) -> Int) = validation {
+    length <= max(this)
 }
-
-//generates
-
-sealed class PasswordViolations {
-    data class TooShort(val min: Int)
-    object ContainsNoDigits
-}
-
-//also generates top-level `validate` and other helper functions
-
-/*...*/
 
 interface HasV {
     val v: String
@@ -65,7 +86,7 @@ fun ConstraintBuilder<VSauce>.customScriptWithoutInterface() = script {
 
 data class VSauce(override val v: String) : HasV {
     companion object : Constrains<VSauce> {
-        override val constraints = constraints {
+        override val constraints by describe {
             constraint(violation = "ShortV") {
                 customScript()
                 customScriptWithoutInterface()
