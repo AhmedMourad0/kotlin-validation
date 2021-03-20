fe218f530eedaafe63d33e0b24d0633455b0e3d7
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2021-03-20 02:42:38 +0200

    Added the use-experimental compiler flag

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -13,3 +13,14 @@ dependencies {
     implementation(kotlin("stdlib"))
     testImplementation("junit:junit:4.12")
 }
+
+val compileKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by project
+val compileTestKotlin: org.jetbrains.kotlin.gradle.tasks.KotlinCompile by project
+compileKotlin.kotlinOptions {
+    freeCompilerArgs = listOf("-Xuse-experimental=kotlin.Experimental")
+//    freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+}
+compileTestKotlin.kotlinOptions {
+    freeCompilerArgs = listOf("-Xuse-experimental=kotlin.Experimental")
+//    freeCompilerArgs = listOf("-Xopt-in=kotlin.RequiresOptIn")
+}
