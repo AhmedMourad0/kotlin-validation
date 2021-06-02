255d1c60a2df2a9f2c151a89c8fdfa6305d88305
Ahmed Mourad <dev.ahmedmourad73744@gmail.com>
2021-06-03 00:03:48 +0200

    Removed redundant dependencies

--- framework/core/build.gradle.kts
+++ framework/core/build.gradle.kts
@@ -19,13 +19,12 @@ kotlin {
     sourceSets {
         val commonMain by getting {
             dependencies {
-                implementation(kotlin("stdlib-common"))
+
             }
         }
         val commonTest by getting {
             dependencies {
-                implementation(kotlin("test-common"))
-                implementation(kotlin("test-annotations-common"))
+                implementation(kotlin("test"))
             }
         }
     }
